dda8bd889e50c8f833c178b984f83f7d219ea819
tallapallesandhya <sandhyatallapalli32@gmail.com>
2023-01-06 15:01:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cdnmethod(request):
+    return render(request,'cdnmethod.html')
+def downloadmethod(request):
+    return render(request,'downloadmethod.html')
